65df36bc85c23dbde0ade7d2f7d472bc68afbb3d
nullxjx <thexjx@gmail.com>
2024-12-06 23:53:57 +0800

    bug fix

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -1,5 +1,5 @@
 TARGETARCH=arm64
 image_name="thexjx/build-protobuf:${TARGETARCH}"
 
-docker buildx build --build-arg TARGETARCH=${TARGETARCH} \
+docker buildx build --build-arg TARGETARCH=${TARGETARCH} --load \
   --platform linux/${TARGETARCH} -t "${image_name}" -f Dockerfile .
