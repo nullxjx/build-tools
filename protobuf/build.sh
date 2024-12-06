89ac6cef1a4cf72f8ce5a125921738d50d8ed8da
nullxjx <thexjx@gmail.com>
2024-12-06 21:49:17 +0800

    bug fix

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -1,4 +1,4 @@
 image_name="thexjx/build-protobuf:arm64"
 
-docker build --no-cache --platform linux/arm64 -t "${image_name}" -f Dockerfile-arm64 .
+docker buildx build --no-cache --platform linux/arm64 -t "${image_name}" -f Dockerfile-arm64 .
 # docker build --no-cache --platform linux/armd4 -t "${image_name}" -f Dockerfile-amd64 .
